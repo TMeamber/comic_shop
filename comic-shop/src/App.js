88536979d91c745084065999a80ca279466f486b
Travis Meamber <tmeamber623@gmail.com>
2018-02-02 09:47:43 -0800

    Added Home page component and having that be the exact path.

--- comic-shop/src/App.js
+++ comic-shop/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Products from './components/Products';
-import About_us from './components/About_us.js';
+import About_us from './components/About_us';
+import Home from './components/Home';
 import { Route, Link } from 'react-router-dom';
 
 
@@ -17,6 +18,7 @@ class App extends Component {
         <Link to='/About_us'>
         <div className='About_us'>About Us</div>
         </Link>
+        <Route exact path='/' component={Home}/>
         <Route path='/Products' component={Products}/>
         <Route path='/About_Us' component={About_us}/>
       </div>
