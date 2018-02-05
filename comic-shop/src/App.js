add3dfc8efb63b1ddadb4fece0219ae6e3a3b2db
Travis Meamber <tmeamber623@gmail.com>
2018-02-05 09:29:33 -0800

    contact component made, added contact us to nav bar.

--- comic-shop/src/App.js
+++ comic-shop/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Products from './components/Products';
 import About_us from './components/About_us';
 import Home from './components/Home';
+import Contact_us from './components/Contact_us';
 import { Route, Link } from 'react-router-dom';
 
 
@@ -11,6 +12,7 @@ import { Route, Link } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
+      <div className='logo'>
       <div className="navbar">
         <Link to='/'>
         <div className='Home'>Home</div>
@@ -21,10 +23,15 @@ class App extends Component {
         <Link to='/About_us'>
         <div className='About_us'>About Us</div>
         </Link>
+        <Link to='/Contact_us'>
+        <div className='Contact_us'>Contact Us</div>
+        </Link>
         <div className='routes'>
         <Route exact path='/' component={Home}/>
         <Route path='/Products' component={Products}/>
         <Route path='/About_Us' component={About_us}/>
+        <Route path='/Contact_us' componet={Contact_us}/>
+        </div>
         </div>
       </div>
     );
---
+++ comic-shop/src/components/Contact_us.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Contact_us.css';
+
+
+export default class Contact_us extends Component{
+    render () {
+        return(
+            <div>
+               
+            </div>
+        )
+    }
+}
