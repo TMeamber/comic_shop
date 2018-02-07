cb63734c4b8ab52b2c3f82459812a667f832a156
Travis Meamber <tmeamber623@gmail.com>
2018-02-06 16:11:05 -0800

    added images for various products and used css to adjust to look better

--- comic-shop/src/components/Contact_us.js
+++ comic-shop/src/components/Contact_us.js
@@ -9,9 +9,9 @@ export default class Contact_us extends Component{
             <div>
                 <img className='CC' alt='' src={CC}/> 
                 <div className='info'>
-               <p>Contact us with our number at  (530) 222-8750</p>
+               <p>Contact us at (530) 222-8750</p>
                <p>Come see us at our location at 1714 Churn Creek Rd, Redding, CA 96002</p>
-               <p>Our hours are Open from 10 AM to 6 PM Monday - Saturday and closed Sundays and Major Holidays</p>
+               <p>Our hours are 10 AM to 6 PM Monday - Saturday and closed Sundays and Major Holidays</p>
                </div>
             </div>
         )
--- comic-shop/src/components/Products.js
+++ comic-shop/src/components/Products.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './Products.css';
-
+import comics from './flashpoint.jpg';
+import figures from './figures.jpg';
+import vinyl from './vinyl.jpg';
+import cards from './cards.jpg';
 
 export default class Products extends Component{
     render () {
         return(
             <div>
-                
+                <div className='products'>
+                <img className='comics' alt='' src={comics}/> 
+                <img className='vinyl' alt='' src={vinyl}/>
+                <img className='cards' alt='' src={cards}/>  
+                <img className='figures' alt='' src={figures}/> 
+                </div>
             </div>
         )
     }
